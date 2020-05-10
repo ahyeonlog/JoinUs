ea375e944ac809310aa2cf1d0fc37471231620eb
dazeddd <joonsoolee14@gmail.com>
2020-05-10 11:39:19 +0900

    Initial Commit

---
+++ JoinUs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JoinUs
+//
+//  Created by 이준수 on 2020/05/10.
+//  Copyright © 2020 iosNowon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
